b79559c090e9c864d03162f929922e04fccef3b1
akindoju <davidd.akindoju@gmail.com>
2023-05-04 18:26:37 +0100

    Expanded on utility classes

--- Figma - CSS/main.js
+++ Figma - CSS/main.js
@@ -1,9 +1,11 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryHeader = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
 });
